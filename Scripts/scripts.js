ea077a9e0a21c0c38e45f55b5e3a3e8b1dc7e0ab
Sai Pc <schand31@asu.edu>
2016-02-04 16:30:26 -0700

    operators

--- Scripts/scripts.js
+++ Scripts/scripts.js
@@ -1,3 +1,20 @@
 /**
  * Created by schandramouli on 2/3/16.
  */
+
+
+// basic operators
+var x = 6 + 7;
+console.log(x);
+var y = 13 - 5;
+console.log(y);
+var z = (x / y) + (x * y);
+console.log(z);
+
+// bitwise --> DO NOT USE THESE, THEY ARE SLOW
+var a = 6 ^ 7;
+console.log(a);
+var b = ~ 5;
+console.log(b);
+var c = (b & a) + (b | a);
+console.log(c);
