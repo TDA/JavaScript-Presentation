3e11e618f7a7151db3e6da790e61c0eb4cb83a45
Sai Pc <schand31@asu.edu>
2016-02-05 13:14:05 -0700

    polyfilling selectors continued

--- Scripts/jquery-polyfill.js
+++ Scripts/jquery-polyfill.js
@@ -3,10 +3,25 @@
  */
 
 function $(element) {
-  if (document.querySelectorAll) {
+  if (!document.querySelectorAll) {
     return document.querySelectorAll(element);
   } else {
     // means theres no builtin support for
     // querySelector engine
+    // first, we need to check if its an id
+    if (element.charAt(0) === '#') {
+      // we found an id
+      // console.log(element.slice(1));
+      return document.getElementById(element.slice(1));
+    } else if (element.charAt(0) === '.') {
+      // means we got a class
+      return document.getElementsByClassName(element.slice(1));
+    }
   }
 }
+
+window.addEventListener("load", function() {
+  //alert("hi");
+  console.log("hello");
+  console.log($("#sai"));
+});
