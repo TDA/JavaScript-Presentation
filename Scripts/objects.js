cdd0e284dc8cb15acdd6b9bde1fb24006478d050
Sai Pc <schand31@asu.edu>
2016-02-04 21:08:49 -0700

    objects

--- Scripts/objects.js
+++ Scripts/objects.js
@@ -6,3 +6,28 @@
 
 var obj = {};
 console.log(typeof obj);
+
+// This IS JSON :D
+var complexObj = {
+  "key1": "value1",
+  "key2": "value2",
+  "key3": "value3"
+};
+
+console.log(typeof complexObj);
+
+var complexObj2 = {
+  key1: "value1",
+  key2: "value2",
+  key3: "value3"
+};
+
+// Accessing object properties
+console.log(complexObj2.key1);
+console.log(complexObj.key1);
+
+// Also,
+console.log(complexObj2["key1"]);
+
+// But not this:
+console.log(complexObj2[key1]);
