5ac5b403dd0fe14e3e2aa165a25163073968a79c
Sai Pc <schand31@asu.edu>
2016-02-04 21:44:54 -0700

    closures

--- Scripts/functions.js
+++ Scripts/functions.js
@@ -2,3 +2,42 @@
  * Created by schandramouli on 2/4/16.
  */
 
+// regular function
+function add(x, y) {
+  return x + y;
+}
+
+console.log(add(3, 5));
+
+// function expression
+var x = function y() {
+  return 8;
+};
+console.log(x);
+console.log(x());
+
+// function literal
+var fLiteral = function() {
+  return 11;
+};
+console.log(fLiteral);
+console.log(fLiteral());
+
+
+
+// LEXICAL SCOPING
+//function closure() {
+//  // emulate private variables
+//  var x = 1;
+//  return function() {
+//    return x++;
+//  }
+//}
+//
+//c = closure();
+//console.log(c());
+//console.log(c());
+//console.log(c());
+//console.log(c());
+
+// Advanced closures
